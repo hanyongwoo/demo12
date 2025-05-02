812c2c2a656e9c90d603b167ff9861e49185e8ca
sinsata-company <shinsata1650@gmail.com>
2025-05-02 12:13:54 +0900

    update from feature/ywhan

--- src/main/java/com/nanumsem/demo12/Demo12Application.java
+++ src/main/java/com/nanumsem/demo12/Demo12Application.java
@@ -8,7 +8,7 @@ public class Demo12Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Demo12Application.class, args);
-        System.out.println("hello");
+        System.out.println("hello ywhan");
     }
 
 }
